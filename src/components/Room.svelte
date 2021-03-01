645b7c1eec7ae525ad217939b1f66157e3ccabaa
Joern Bernhardt <joern.bernhardt@compose.us>
2021-03-01 01:46:14 +0100

    Add possible stages in a room
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/components/Room.svelte
+++ src/components/Room.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
   export let name: string;
+
+  let stage: string = "start";
 </script>
 
 <h1>Room {name}</h1>
+
+{#if stage === "start"}
+  <h2>waiting for players to join</h2>
+{:else if stage === "draw"}
+  <h2>waiting for players to draw</h2>
+{:else if stage === "guess"}
+  <h2>waiting for players to guess prompt</h2>
+{:else if stage === "select"}
+  <h2>waiting for players to select</h2>
+{:else if stage === "rate"}
+  <h2>waiting for players to rate prompts</h2>
+{:else if stage === "scores"}
+  <h2>showing scores to players</h2>
+{/if}
