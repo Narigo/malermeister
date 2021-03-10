470f1cbedbb986f0fee4b2a3a5c508dfc5628499
Joern Bernhardt <joern.bernhardt@compose.us>
2021-03-10 01:46:53 +0100

    Add current player to list of players
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/service/player-store.ts
+++ src/service/player-store.ts
@@ -2,9 +2,10 @@ import { writable } from "svelte/store";
 import generatePlayer from "./generate-player";
 export type Player = { name: string };
 
+const currentPlayer = generatePlayer();
 const players = writable<{ players: Player[]; currentPlayer: Player }>({
-  players: [],
-  currentPlayer: generatePlayer(),
+  players: [currentPlayer],
+  currentPlayer,
 });
 
 export default players;
