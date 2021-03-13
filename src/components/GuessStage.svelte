e2a77ab101e549ba272de71cc3e9e43e4c9f074a
Joern Bernhardt <joern.bernhardt@compose.us>
2021-03-13 01:26:51 +0100

    Make game run through all stages
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/components/GuessStage.svelte
+++ src/components/GuessStage.svelte
@@ -12,38 +12,36 @@
   export let done: () => void;
 
   let stage: GuessStage = "guess";
+  let currentDrawingIndex = 0;
+  let drawing: Drawing = $drawings[currentDrawingIndex];
 
-  function addGuesses(drawing: Drawing) {
-    return () => {
-      const index = $drawings.findIndex((d) => d === drawing);
-      $drawings = [
-        ...$drawings.slice(0, index),
-        {
-          ...drawing,
-          guesses: $playerStore.players.map((p) => ({
-            player: p,
-            prompt: generatePrompt(),
-          })),
-        },
-        ...$drawings.slice(index + 1),
-      ];
-      stage = "select";
-    };
+  function addGuesses() {
+    const index = $drawings.findIndex((d) => d === drawing);
+    $drawings = [
+      ...$drawings.slice(0, index),
+      {
+        ...drawing,
+        guesses: $playerStore.players.map((p) => ({
+          player: p,
+          prompt: generatePrompt(),
+        })),
+      },
+      ...$drawings.slice(index + 1),
+    ];
+    stage = "select";
   }
 
-  function selectPrompt(drawing: Drawing) {
-    return () => {
-      stage = "rate";
-    };
+  function selectPrompt() {
+    stage = "rate";
   }
 
-  function ratePrompts(drawing: Drawing) {
-    return () => {
-      if (drawing === $drawings[$drawings.length - 1]) {
-        return done();
-      }
-      stage = "guess";
-    };
+  function ratePrompts() {
+    currentDrawingIndex = currentDrawingIndex + 1;
+    if ($drawings.length <= currentDrawingIndex) {
+      return done();
+    }
+    drawing = $drawings[currentDrawingIndex];
+    stage = "guess";
   }
 </script>
 
@@ -52,28 +50,22 @@
     <li>{drawing.prompt} by {drawing.author.name}</li>
   {/each}
 </ul>
-{#each $drawings as drawing}
-  {#if stage === "guess"}
-    <h2>
-      waiting for players to guess prompts for drawing {drawing.prompt} by {drawing
-        .author.name}
-    </h2>
-    <button on:click={addGuesses(drawing)}
-      >Add guesses for {drawing.prompt}</button
-    >
-  {:else if stage === "select"}
-    <h2>
-      waiting for players to select prompt for drawing {drawing.prompt} by {drawing
-        .author.name}
-    </h2>
-    <button on:click={selectPrompt(drawing)}
-      >Everyone selected their guess!</button
-    >
-  {:else if stage === "rate"}
-    <h2>
-      waiting for players to rate prompt for drawing {drawing.prompt} by {drawing
-        .author.name}
-    </h2>
-    <button on:click={ratePrompts(drawing)}>Everybody rated</button>
-  {/if}
-{/each}
+{#if stage === "guess"}
+  <h2>
+    waiting for players to guess prompts for drawing {drawing.prompt} by {drawing
+      .author.name}
+  </h2>
+  <button on:click={addGuesses}>Add guesses for {drawing.prompt}</button>
+{:else if stage === "select"}
+  <h2>
+    waiting for players to select prompt for drawing {drawing.prompt} by {drawing
+      .author.name}
+  </h2>
+  <button on:click={selectPrompt}>Everyone selected their guess!</button>
+{:else if stage === "rate"}
+  <h2>
+    waiting for players to rate prompt for drawing {drawing.prompt} by {drawing
+      .author.name}
+  </h2>
+  <button on:click={ratePrompts}>Everybody rated</button>
+{/if}
