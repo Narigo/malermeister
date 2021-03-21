4d4bd751bab53311b730321306c2e2a582e48078
Joern Bernhardt <joern.bernhardt@compose.us>
2021-03-21 13:40:12 +0100

    Add a canvas element
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/components/DrawCanvas.svelte
+++ src/components/DrawCanvas.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
   export let prompt;
+
+  let canvasRef: HTMLCanvasElement;
 </script>
 
 <p>There will be a canvas to draw '{prompt}'</p>
+<canvas bind:this={canvasRef} />
 
 <style>
   p {
     color: green;
+  }
+
+  canvas {
+    box-shadow: 0 0 20px #666;
+    display: block;
+    height: 50%;
+    margin: 25px auto;
+    width: 50%;
   }
 </style>
