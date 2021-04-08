ee1dda233fb5d7874577c03d4f6448d667e4696a
Joern Bernhardt <joern.bernhardt@compose.us>
2021-04-08 12:28:14 +0200

    Draw pencil
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/components/Pencil.svelte
+++ src/components/Pencil.svelte
@@ -7,11 +7,27 @@
 
 <svg {width} {height} viewBox="0 0 100 100" on:click>
   <path
-    d="M 25 75 L 75 50"
+    d="M 25 75 L 60 60"
     stroke-width={strokeWidth}
     stroke-linecap="round"
     fill="none"
     stroke={color}
+    vector-effect="non-scaling-stroke"
+  />
+  <path
+    d="M 60 60 L 50 30 L 55 0 M 60 60 L 80 35 L 90 0"
+    stroke-width="1"
+    stroke-linecap="round"
+    fill="none"
+    stroke="#000"
+    vector-effect="non-scaling-stroke"
+  />
+  <path
+    d="M 60 60 L 50 30 L 80 35 L 60 60"
+    stroke-width="1"
+    stroke-linecap="round"
+    fill={color}
+    stroke="#000"
     vector-effect="non-scaling-stroke"
   />
 </svg>
